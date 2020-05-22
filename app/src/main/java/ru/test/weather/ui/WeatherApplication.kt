7b87016c17bb45bf6ac490972f709900398c55bb
a.padafa@virtuumlab.ru <a.padafa@virtuumlab.ru>
2020-05-22 18:19:18 +0300

    init, add splash

---
+++ app/src/main/java/ru/test/weather/ui/WeatherApplication.kt
@@ -0,0 +1,11 @@
+package ru.test.weather.ui
+
+import android.app.Application
+
+class WeatherApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ app/src/main/java/ru/test/weather/ui/views/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.test.weather.ui.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.test.weather.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
