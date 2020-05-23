8fac7e6fe5ebc889cd5329502da8dce22d0ade6a
PAlAl <dtritus93@yandex.ru>
2020-05-23 12:10:37 +0300

    ref status bar and splash

--- app/src/main/java/ru/test/weather/ui/views/SplashActivity.kt
+++ app/src/main/java/ru/test/weather/ui/views/SplashActivity.kt
@@ -5,9 +5,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //startActivity(Intent(this, MainActivity::class.java))
-        //finish()
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
